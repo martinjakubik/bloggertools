3623028810e26c03b8dc05d0c71425dbfb4a8b39
martin jakubik <martin.jakubik@sap.com>
2025-08-12 10:44:59 +0200

    added flags

--- merge-exported-google-photos.sh
+++ merge-exported-google-photos.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # sets up usage
-USAGE="usage: $0 -i --inputDir inputDir -o --destinationDir destinationDir -d --debug"
+USAGE="usage: $0 -i --inputDir inputDir -o --destinationDir destinationDir --listDirectories yes|no --copyFiles yes|no -d --debug"
 
 # set up defaults
 DEBUG=0
+list_numbered_directories=1
+copy_files=1
 using_downloads_folder=1
 inputDir=~/inputDir
 destinationDir=~/destinationDir
@@ -22,6 +24,8 @@ do
     (--inputDir) inputDir="$2"; shift;;
     (-o) destinationDir="$2"; shift;;
     (--destinationDir) destinationDir="$2"; shift;;
+    (--listDirectories) listDirectories="$2"; shift;;
+    (--copyFiles) copyFiles="$2"; shift;;
     (-d) DEBUG=1;;
     (--debug) DEBUG=1;;
     (-*) echo >&2 ${USAGE}
@@ -31,31 +35,44 @@ do
 done
 
 echo you entered values
-echo   "From inputDir : $inputDir"
-echo   "To            : $destinationDir"
+echo   "From inputDir     : $inputDir"
+echo   "To                : $destinationDir"
+echo   "List directories  : $listDirectories"
+echo   "Copy files        : $copyFiles"
 
-echo > list_of_google_takeout_directories
-
-if [ $using_downloads_folder -eq 1 ] ; then
+if [ "$using_downloads_folder" -eq 1 ] ; then
     basefilepath=~/Downloads
 else
     basefilepath=$(dirname "$inputDir")/$(basename "$inputDir")
 fi
 
-for google_takeout_directory in $basefilepath/Takeout* ; do
-    if [[ -d "$google_takeout_directory" ]] ; then
-        find "$google_takeout_directory" -type d -depth 2 >> list_of_google_takeout_directories
-    fi
-done
+if [ "$listDirectories" = "no" ] ; then
+    list_numbered_directories=0
+fi
 
-if [[ -e list_of_google_takeout_directories ]] ; then
-    while IFS= read -r google_takeout_source_directory; do
-        if [[ ! -z "${google_takeout_source_directory// }" ]] ; then
-            google_takeout_directory_basename=$(basename "$google_takeout_source_directory")
-            if [[ ! -d $destinationDir/"$google_takeout_directory_basename" ]] ; then
-                mkdir -p "$destinationDir/$google_takeout_directory_basename"
-            fi
-            cp "$google_takeout_source_directory"/* "$destinationDir"/"$google_takeout_directory_basename/"
+if [ "$copyFiles" = "no" ] ; then
+    copy_files=0
+fi
+
+if [[ $list_numbered_directories -eq 1 ]] ; then
+    echo > list_of_numbered_directories
+    for numbered_directory in $basefilepath/Takeout* ; do
+        if [[ -d "$numbered_directory" ]] ; then
+            find "$numbered_directory" -type d -depth 2 >> list_of_numbered_directories
         fi
-    done < list_of_google_takeout_directories
+    done
+fi
+
+if [[ $copy_files -eq 1 ]] ; then
+    if [[ -e list_of_numbered_directories ]] ; then
+        while IFS= read -r numbered_source_directory; do
+            if [[ ! -z "${numbered_source_directory// }" ]] ; then
+                numbered_directory_basename=$(basename "$numbered_source_directory")
+                if [[ ! -d $destinationDir/"$numbered_directory_basename" ]] ; then
+                    echo mkdir -p "$destinationDir/$numbered_directory_basename"
+                fi
+                echo cp "$numbered_source_directory"/* "$destinationDir"/"$numbered_directory_basename/"
+            fi
+        done < list_of_numbered_directories
+    fi
 fi
